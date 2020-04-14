bfb022489b0d7818357c586463f2eecb7069c51c
Andrei Mihaescu <andrei.mihaescu91@gmail.com>
2020-04-14 10:32:55 +0300

    finalized server added some public

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const database = require('./db/db-connection')
-const cards_service = require('./cards/cards-service')
-const board_service = require('./boards/board-service')
+const database = require('./server/db/db-connection')
+const cards_service = require('./server/cards/cards-service')
+const board_service = require('./server/boards/board-service')
 const express = require('express')
 
 const port = 3000
@@ -8,6 +8,7 @@ const port = 3000
 database.db_init().then(console.log("Connected to database"))
 app = express()
 app.use(express.json())
+app.use(express.static('public'))
 board_service.init(app)
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
